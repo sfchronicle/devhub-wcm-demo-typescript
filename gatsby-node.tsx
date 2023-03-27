27e0a341fcc4d2181d5c683f072af20a0d5fd3c2
yzhao <ying.zhao@sfchronicle.comm>
2023-03-27 14:40:25 -0700

    from js to tsx

--- gatsby-node.tsx
+++ gatsby-node.tsx
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- var {requestImage} = require("./tasks/node-helpers");
+var { requestImage } = require('./tasks/node-helpers')
 
 // Import all the WCM photos you'd like to use in this project
 // NOTE: Leave this as an empty array if you aren't importing any WCM photos, but you won't be able to use the WCMImage component
@@ -19,7 +19,7 @@ exports.sourceNodes = async ({
   const { createNode } = actions
 
   // Request and push photo data into an array
-  const photoData = [];
+  const photoData = []
   wcmPhotos.forEach((url) => {
     photoData.push(requestImage(url))
   })
@@ -27,7 +27,7 @@ exports.sourceNodes = async ({
   // Take the ratios/ids turn them into graphql nodes
   await Promise.all(photoData).then((values) => {
     values.forEach((photo) => {
-      const type = "wcmPhotos";
+      const type = 'wcmPhotos'
       createNode({
         photo,
         id: createNodeId(`${type}${photo.wcmid}`),
